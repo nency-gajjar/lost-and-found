0c6e17acd56ce919452a22f35f4176afeb72bcea
nency-gajjar <nency.bhadeshiya@bacancy.com>
2022-10-11 12:56:45 +0530

    add pages for UI

---
+++ pages/preview.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper">
+    <div class="card">
+      <h1 class="title">Preview</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.wrapper {
+  @apply min-h-screen flex justify-center items-end text-center mx-auto;
+}
+.card {
+  @apply p-10 rounded-lg text-indigo-500 bg-indigo-100 shadow-lg;
+  .title {
+    @apply text-6xl font-bold;
+  }
+}
+</style>
