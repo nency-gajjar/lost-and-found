ce0f13d7adca842878a3fdf54775ddd308ed92a0
nency-gajjar <nency.bhadeshiya@bacancy.com>
2022-10-18 12:51:27 +0530

    Ui update

--- pages/found-items/index.vue
+++ pages/found-items/index.vue
@@ -27,7 +27,7 @@
           + Add New Item
         </button>
       </div>
-      <div v-if="!isLoading && lostItems.length > 0" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <!-- <div v-if="!isLoading && lostItems.length > 0" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <div v-for="item in lostItems" :key="item.id" @click="viewItem(item)" class="
             cursor-pointer
             py-4
@@ -74,6 +74,274 @@
       <div v-else>
         <div wire:loading class="loader-container z-50 overflow-hidden flex flex-col items-center justify-center">
           <div class="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+        </div>
+      </div> -->
+
+    </div>
+    <hr class="m-10" />
+    <div>
+      <h2 class="text-2xl font-semibold leading-tight">Found Items</h2>
+    </div>
+    <div class="my-2 flex sm:flex-row flex-col">
+      <div class="flex flex-row mb-1 sm:mb-0">
+        <div class="relative">
+          <select
+            class="appearance-none h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+            <option>5</option>
+            <option>10</option>
+            <option>20</option>
+          </select>
+          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+            </svg>
+          </div>
+        </div>
+        <div class="relative">
+          <select
+            class="appearance-none h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
+            <option>All</option>
+            <option>Claimed</option>
+            <option>Unclaimed</option>
+          </select>
+          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div class="block relative">
+        <span class="h-full absolute inset-y-0 left-0 flex items-center pl-2">
+          <svg viewBox="0 0 24 24" class="h-4 w-4 fill-current text-gray-500">
+            <path
+              d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z">
+            </path>
+          </svg>
+        </span>
+        <input placeholder="Search"
+          class="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
+      </div>
+    </div>
+    <div class="overflow-x-auto">
+      <div
+        class="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center bg-gray-100 font-sans overflow-hidden">
+        <div class="w-full">
+          <div class="bg-white shadow-md rounded my-6">
+            <table class="min-w-max w-full table-auto">
+              <thead>
+                <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                  <th data-priority="1" class="py-3 px-6 text-left">Item</th>
+                  <th class="py-3 px-6 text-left">Found Date</th>
+                  <th class="py-3 px-6 text-center">Weight</th>
+                  <th class="py-3 px-6 text-center">Dimensions</th>
+                  <th class="py-3 px-6 text-center">Status</th>
+                  <th class="py-3 px-6 text-center">Actions</th>
+                </tr>
+              </thead>
+              <tbody class="text-gray-600 text-sm font-light">
+                <tr class="border-b border-gray-200 hover:bg-gray-100">
+                  <td class="py-3 px-6 text-left">
+                    <div class="flex items-center">
+                      <div class="mr-2">
+                        <img class="w-6 h-6 rounded-full" src="@/assets/images/headphones.png" />
+                      </div>
+                      <span>Headphone</span>
+                    </div>
+                  </td>
+                  <td class="py-3 px-6 text-left">
+                    <span>22-10-2022</span>
+                  </td>
+                  <td class="py-3 px-6 text-left">
+                    <span>0.11 Pounds</span>
+                  </td>
+                  <td class="py-3 px-6 text-left">
+                    <span>1.9 X 3.9 inch</span>
+                  </td>
+                  <td class="py-3 px-6 text-center">
+                    <span class="bg-purple-200 text-purple-600 py-1 px-3 rounded-full text-xs">Unclaimed</span>
+                  </td>
+                  <td class="py-3 px-6 text-center">
+                    <div class="flex item-center justify-center">
+                      <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                        </svg>
+                      </div>
+                      <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                        </svg>
+                      </div>
+                      <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                        </svg>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+                <tr class="border-b border-gray-200 hover:bg-gray-100">
+                  <td class="py-3 px-6 text-left">
+                    <div class="flex items-center">
+                      <div class="mr-2">
+                        <img class="w-6 h-6 rounded-full" src="@/assets/images/headphones.png" />
+                      </div>
+                      <span>Headphone</span>
+                    </div>
+                  </td>
+                  <td class="py-3 px-6 text-left">
+                    <span>22-10-2022</span>
+                  </td>
+                  <td class="py-3 px-6 text-left">
+                    <span>0.11 Pounds</span>
+                  </td>
+                  <td class="py-3 px-6 text-left">
+                    <span>1.9 X 3.9 inch</span>
+                  </td>
+                  <td class="py-3 px-6 text-center">
+                    <span class="bg-red-200 text-red-600 py-1 px-3 rounded-full text-xs">Pending</span>
+                  </td>
+                  <td class="py-3 px-6 text-center">
+                    <div class="flex item-center justify-center">
+                      <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                        </svg>
+                      </div>
+                      <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                        </svg>
+                      </div>
+                      <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                        </svg>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+                <tr class="border-b border-gray-200 hover:bg-gray-100">
+                  <td class="py-3 px-6 text-left">
+                    <div class="flex items-center">
+                      <div class="mr-2">
+                        <img class="w-6 h-6 rounded-full" src="@/assets/images/headphones.png" />
+                      </div>
+                      <span>Headphone</span>
+                    </div>
+                  </td>
+                  <td class="py-3 px-6 text-left">
+                    <span>22-10-2022</span>
+                  </td>
+                  <td class="py-3 px-6 text-left">
+                    <span>0.11 Pounds</span>
+                  </td>
+                  <td class="py-3 px-6 text-left">
+                    <span>1.9 X 3.9 inch</span>
+                  </td>
+                  <td class="py-3 px-6 text-center">
+                    <span class="bg-purple-200 text-purple-600 py-1 px-3 rounded-full text-xs">Unclaimed</span>
+                  </td>
+                  <td class="py-3 px-6 text-center">
+                    <div class="flex item-center justify-center">
+                      <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                        </svg>
+                      </div>
+                      <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                        </svg>
+                      </div>
+                      <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                        </svg>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+                <tr class="border-b border-gray-200 hover:bg-gray-100">
+                  <td class="py-3 px-6 text-left">
+                    <div class="flex items-center">
+                      <div class="mr-2">
+                        <img class="w-6 h-6 rounded-full" src="@/assets/images/headphones.png" />
+                      </div>
+                      <span>Headphone</span>
+                    </div>
+                  </td>
+                  <td class="py-3 px-6 text-left">
+                    <span>22-10-2022</span>
+                  </td>
+                  <td class="py-3 px-6 text-left">
+                    <span>0.11 Pounds</span>
+                  </td>
+                  <td class="py-3 px-6 text-left">
+                    <span>1.9 X 3.9 inch</span>
+                  </td>
+                  <td class="py-3 px-6 text-center">
+                    <span class="bg-red-200 text-red-600 py-1 px-3 rounded-full text-xs">Pending</span>
+                  </td>
+                  <td class="py-3 px-6 text-center">
+                    <div class="flex item-center justify-center">
+                      <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                        </svg>
+                      </div>
+                      <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                        </svg>
+                      </div>
+                      <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                        </svg>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+
+              </tbody>
+            </table>
+            <div
+              class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
+              <span class="text-xs xs:text-sm text-gray-900">
+                Showing 1 to 4 of 50 Entries
+              </span>
+              <div class="inline-flex mt-2 xs:mt-0">
+                <button class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l">
+                  Prev
+                </button>
+                <button class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-r">
+                  Next
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
