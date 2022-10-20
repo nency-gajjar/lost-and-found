04e3149bdf0b09512756e94273ed570121764f92
nency-gajjar <nency.bhadeshiya@bacancy.com>
2022-10-21 01:36:13 +0530

    Carousel UI fixes, revert changesof Item detail form

--- pages/index.vue
+++ pages/index.vue
@@ -2,164 +2,263 @@
   <div>
     <div class="wrapper border-t min-h-max">
       <div class="pt-4 gradient w-full">
-        <div class="container mx-auto px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+        <div
+          class="
+            container
+            mx-auto
+            px-3
+            mx-auto
+            flex flex-wrap flex-col
+            md:flex-row
+            items-center
+          "
+        >
           <!--Left Col-->
-          <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
+          <div
+            class="
+              flex flex-col
+              w-full
+              md:w-2/5
+              justify-center
+              items-start
+              text-center
+              md:text-left
+            "
+          >
             <div class="flex">
               <div class="qr-code mr-3 md:mr-6 lg:mr-12">
-                <img class="bg-white rounded-lg p-4" src="@/assets/images/found-items-qr.png" alt="qr code image" />
-                <p class="text-white text-sm mt-4">Scan this QR to view Lost Items</p>
+                <img
+                  class="bg-white rounded-lg p-4"
+                  src="@/assets/images/found-items-qr.png"
+                  alt="qr code image"
+                />
+                <p class="text-white text-sm mt-4">
+                  Scan this QR to view Lost Items
+                </p>
               </div>
               <div class="qr-code">
-                <img class="bg-white rounded-lg p-4" src="@/assets/images/item-details-qr.png" alt="qr code image" />
-                <p class="text-white text-sm mt-4">Scan this QR to Add new lost item</p>
-
+                <img
+                  class="bg-white rounded-lg p-4"
+                  src="@/assets/images/item-details-qr.png"
+                  alt="qr code image"
+                />
+                <p class="text-white text-sm mt-4">
+                  Scan this QR to Add new lost item
+                </p>
               </div>
             </div>
-
           </div>
 
           <!--Right Col-->
           <div class="w-full md:w-3/5 text-center pb-10">
-            <img class="w-full md:w-4/5 mx-auto z-50" src="https://www.tailwindtoolbox.com/templates/hero.png">
+            <img
+              class="w-full md:w-4/5 mx-auto z-50"
+              src="https://www.tailwindtoolbox.com/templates/hero.png"
+            />
           </div>
         </div>
         <div class="relative -mt-12 lg:-mt-20">
-          <svg viewBox="0 0 1428 174" version="1.1" xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink">
+          <svg
+            viewBox="0 0 1428 174"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+          >
             <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-              <g transform="translate(-2.000000, 44.000000)" fill="#FFFFFF" fill-rule="nonzero">
+              <g
+                transform="translate(-2.000000, 44.000000)"
+                fill="#FFFFFF"
+                fill-rule="nonzero"
+              >
                 <path
                   d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496"
-                  opacity="0.100000001"></path>
+                  opacity="0.100000001"
+                ></path>
                 <path
                   d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z"
-                  opacity="0.100000001"></path>
+                  opacity="0.100000001"
+                ></path>
                 <path
                   d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z"
-                  id="Path-4" opacity="0.200000003"></path>
+                  id="Path-4"
+                  opacity="0.200000003"
+                ></path>
               </g>
-              <g transform="translate(-4.000000, 76.000000)" fill="#FFFFFF" fill-rule="nonzero">
+              <g
+                transform="translate(-4.000000, 76.000000)"
+                fill="#FFFFFF"
+                fill-rule="nonzero"
+              >
                 <path
-                  d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z">
-                </path>
+                  d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z"
+                ></path>
               </g>
             </g>
           </svg>
         </div>
       </div>
     </div>
-    <div class="flex justify-center my-5">
-      <VueSlickCarousel class="w-5/6" v-bind="sliderSetting">
-        <div>
-          <div class="sliderCard">
-            <img class="
-                w-full
-                rounded-t-lg
-                md:h-auto md:w-48 md:rounded-none md:rounded-l-lg
-              " src="@/assets/images/not-found.png" alt="" />
-              <strong>Laptop</strong>
-              <p>Unclaimmed</p>
-          </div>
+    <section class="bg-white border-b py-8">
+      <div class="container max-w-6xl mx-auto m-8">
+        <h3
+          class="
+            w-full
+            my-2
+            text-2xl
+            font-bold
+            leading-tight
+            text-center text-gray-700
+          "
+        >
+          Recently Found Items
+        </h3>
+        <div class="flex justify-center">
+          <span
+            class="
+              w-20
+              border-t-4 border-solid border-orange-200
+              inline-block
+              mb-3
+            "
+          ></span>
         </div>
-        <div>
-          <div class="sliderCard">
-            <img class="
-                w-full
-                rounded-t-lg
-                md:h-auto md:w-48 md:rounded-none md:rounded-l-lg
-              " src="@/assets/images/not-found.png" alt="" />
-              <strong>Laptop</strong>
-              <p>Unclaimmed</p>
-          </div>
+        <div class="flex justify-center my-5">
+          <VueSlickCarousel class="w-5/6" v-bind="sliderSetting">
+            <div v-for="item in 5" :key="item">
+              <div class="sliderCard cursor-pointer shadow-md border">
+                <img
+                  class="
+                    object-cover
+                    w-full
+                    rounded-t-lg
+                    md:h-32 md:w-32
+                    sm:h-16 sm:w-16
+                    md:rounded-none md:rounded-l-lg
+                  "
+                  src="@/assets/images/headphones.png"
+                  alt=""
+                />
+                <h5 class="m-4 text-lg font-semibold">Headphone</h5>
+                <ul role="list" class="mb-4 text-left text-gray-500">
+                  <li class="flex items-center space-x-2">
+                    <!-- Icon -->
+                    <svg
+                      class="
+                        flex-shrink-0
+                        w-5
+                        h-5
+                        text-green-500
+                        dark:text-green-400
+                      "
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    <span>Status: Unclaimed</span>
+                  </li>
+                  <li class="flex items-center space-x-2">
+                    <!-- Icon -->
+                    <svg
+                      class="
+                        flex-shrink-0
+                        w-5
+                        h-5
+                        text-green-500
+                        dark:text-green-400
+                      "
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    <span>Found Date: 22-10-2022</span>
+                  </li>
+                </ul>
+                <button
+                  type="submit"
+                  class="
+                    w-full
+                    font-medium
+                    text-sm
+                    px-5
+                    py-2.5
+                    rounded-lg
+                    button
+                    focus:outline-none
+                    focus:ring-2
+                    focus:ring-offset-2
+                    focus:ring-offset-primary-60
+                    transition-all
+                    font-display
+                    disabled:cursor-not-allowed
+                    bg-accent-100
+                    text-white
+                    focus:ring-accent-100
+                    shadow-accent
+                    hover:bg-accent-200
+                  "
+                >
+                  <span class="button__text"> View Item </span>
+                </button>
+                <!-- </div> -->
+              </div>
+            </div>
+          </VueSlickCarousel>
         </div>
-        <div>
-          <div class="sliderCard">
-            <img class="
-                w-full
-                rounded-t-lg
-                md:h-auto md:w-48 md:rounded-none md:rounded-l-lg
-              " src="@/assets/images/not-found.png" alt="" />
-              <strong>Laptop</strong>
-              <p>Unclaimmed</p>
-          </div>
-        </div>
-        <div>
-          <div class="sliderCard">
-            <img class="
-                w-full
-                rounded-t-lg
-                md:h-auto md:w-48 md:rounded-none md:rounded-l-lg
-              " src="@/assets/images/not-found.png" alt="" />
-              <strong>Laptop</strong>
-              <p>Unclaimmed</p>
-          </div>
-        </div>
-        <div>
-          <div class="sliderCard">
-            <img class="
-                w-full
-                rounded-t-lg
-                md:h-auto md:w-48 md:rounded-none md:rounded-l-lg
-              " src="@/assets/images/not-found.png" alt="" />
-              <strong>Laptop</strong>
-              <p>Unclaimmed</p>
-          </div>
-        </div>
-        <div>
-          <div class="sliderCard">
-            <img class="
-                w-full
-                rounded-t-lg
-                md:h-auto md:w-48 md:rounded-none md:rounded-l-lg
-              " src="@/assets/images/not-found.png" alt="" />
-              <strong>Laptop</strong>
-              <p>Unclaimmed</p>
-          </div>
-        </div>
-      </VueSlickCarousel>
-    </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import VueSlickCarousel from 'vue-slick-carousel';
-import 'vue-slick-carousel/dist/vue-slick-carousel.css';
+import VueSlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
 // optional style for arrows & dots
-import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
 export default {
   components: { VueSlickCarousel },
-  data(){
-    return{
+  data() {
+    return {
       sliderSetting: {
-        "infinite": true,
-        "slidesToShow": 3,
-        "slidesToScroll": 1,
-        "autoplay": true,
-        "autoplaySpeed": 2000,
-        "pauseOnDotsHover": true,
-        "pauseOnFocus": true,
-        "pauseOnHover": true,
-        "responsive": [
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnDotsHover: true,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        responsive: [
           {
-            "breakpoint": 600,
-            "settings": {
-              "slidesToShow": 2,
-              "slidesToScroll": 2,
-            }
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            },
           },
           {
-            "breakpoint": 480,
-            "settings": {
-              "slidesToShow": 1,
-              "slidesToScroll": 1
-            }
-          }
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
         ],
-      }
-    }
+      },
+    };
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -185,17 +284,24 @@ export default {
   }
 }
 
-.sliderCard{
-  @apply flex justify-center items-center flex-col bg-white mx-5 p-5 rounded-lg;
+.sliderCard {
+  @apply flex justify-center items-center flex-col bg-gray-100 mx-5 p-5 rounded-lg;
   display: flex !important;
 }
 </style>
 
 <style>
-.slick-prev, .slick-next{
-  height: 18px !important;
+.slick-prev,
+.slick-next {
+  height: 35px !important;
+  width: 35px !important;
   background: none !important;
   border-radius: 60px;
-  background-color: #ff9800 !important;
+  background-color: #f06b04 !important;
+}
+.slick-prev:before,
+.slick-next:before {
+  font-size: 34px;
+  opacity: 1;
 }
 </style>
