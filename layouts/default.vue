76d41075a0b4150cc6f5ee4b648e81159d3007ba
nency-gajjar <nency.bhadeshiya@bacancy.com>
2023-01-11 16:20:06 +0530

    Revert "Showing default vue conditionally"
    This reverts commit 920a694cc3139cf67a57d127f2ddad4ec3c9d557.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,10 @@
 <template>
-  <div class="min-h-screen" v-if="!loading">
+  <div class="min-h-screen">
     <NavBar />
     <Nuxt class="pb-8" />
     <Footer />
   </div>
 </template>
-
-<script>
-export default {
-  data: () => ({
-    loading: true
-  }),
-  created() {
-    this.$nextTick(function() {
-      this.loading = false
-    })
-  }
-}
-</script>
 
 <style>
 html {
