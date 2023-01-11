920a694cc3139cf67a57d127f2ddad4ec3c9d557
nency-gajjar <nency.bhadeshiya@bacancy.com>
2023-01-11 16:10:14 +0530

    Showing default vue conditionally

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,23 @@
 <template>
-  <div class="min-h-screen">
+  <div class="min-h-screen" v-if="!loading">
     <NavBar />
     <Nuxt class="pb-8" />
     <Footer />
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    loading: true
+  }),
+  created() {
+    this.$nextTick(function() {
+      this.loading = false
+    })
+  }
+}
+</script>
 
 <style>
 html {
